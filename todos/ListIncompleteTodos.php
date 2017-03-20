54c96478ffab82c78526c8ae27c3c2a99bc8b3cd
jamcnaughton <james.andrew.mcnaughton@gmail.com>
2017-03-20 20:16:19 +0000

    Part 1
    Additions to make the tool meet the requirements of part 1.

---
+++ todos/ListIncompleteTodos.php
@@ -0,0 +1,38 @@
+<?php
+
+	// Get utility classes.
+	include ('utils/DatabaseConnection.php');
+	
+	// Expecting a GET request - verify this.
+	$method = $_SERVER['REQUEST_METHOD'];
+	if ($method != 'GET') {
+		http_response_code(405);
+		die('Expecting only GET requests to this URI.');
+	}
+	
+	// Get connection to database.
+	$databaseConnection = DatabaseConnection::getConnection();
+	
+	// Build SQL statement for retrieving uncomplete todos.
+	$sqlStatement = 'SELECT todo_id, description FROM todos WHERE status = 0';
+	
+	// Execute the SQL statement.
+	$result = mysqli_query($databaseConnection, $sqlStatement);
+	
+	// Provide an error if the SQL statement failed.
+	if ( !$result ) {
+		http_response_code(503);
+		die($databaseConnection->error);
+	}
+	
+	// Get query results as an array.
+	$resultsArray = array();
+	for ($i = 0; $i < mysqli_num_rows($result); $i++) {
+		$resultsArray[] = mysqli_fetch_object($result);
+	}
+	
+	// Output the results in JSON.
+	echo json_encode($resultsArray);
+
+
+?>
